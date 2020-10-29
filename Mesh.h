1a4b2cd6d1145e4f9f399373181eb07fc9107a80
alfarizi <alfariiiziiiii@gmail.com>
2020-10-29 13:08:14 +0700

    generilize create vertex buffer to create buffer; and again, it's work, but not really efficient

--- Mesh.h
+++ Mesh.h
@@ -38,72 +38,19 @@ public:
 private:
     void CreateVertexBuffer( std::vector<Vertex>& vertices )
     {
-        // buffer info (doesn't include assigning memory)
-        VkBufferCreateInfo bufferInfo{};
-        bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-        bufferInfo.size = static_cast<uint64_t>( sizeof(Vertex) * vertices.size() );
-        bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-        bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-        bufferInfo.queueFamilyIndexCount = 0;       // optional: just for concurent sharing mode
-        bufferInfo.pQueueFamilyIndices = nullptr;   // optional: just for concurent sharing mode
-        if( vkCreateBuffer( _device, &bufferInfo, nullptr, &_vertexBuffer)
-            != VK_SUCCESS )
-        {
-            throw std::runtime_error( "Failed to create vertex buffer!" );
-        }
-
-        // get memory requirements from vertexBuffer
-        VkMemoryRequirements memReq{};
-        vkGetBufferMemoryRequirements( _device, _vertexBuffer, &memReq );
-
-        // memory allocate info
-        VkMemoryAllocateInfo allocateInfo{};
-        allocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-        allocateInfo.allocationSize = memReq.size;
-
-        VkPhysicalDeviceMemoryProperties memProps{};
-        vkGetPhysicalDeviceMemoryProperties( _physicalDevice, &memProps );
-        int32_t memoryType = FindProperties( &memProps, 
-                                            memReq.memoryTypeBits,
-                                            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | 
-                                            VK_MEMORY_PROPERTY_HOST_COHERENT_BIT );
+        size_t bufferSize = sizeof(Vertex) * vertices.size();
+        VkBufferUsageFlags bufferUsage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+        VkMemoryPropertyFlags memPropFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
         
-        allocateInfo.memoryTypeIndex = memoryType;
-        if( vkAllocateMemory( _device, &allocateInfo, nullptr, &_vertexBufferMemory)
-            != VK_SUCCESS )
-        {
-            throw std::runtime_error( "Failed to allocate memory for vertex buffer!" );
-        }
-
-        // binding vertex buffer with the memory allocation
-        vkBindBufferMemory( _device, _vertexBuffer, _vertexBufferMemory, 0 );
+        Buffer::CreateBuffer( _physicalDevice, _device, bufferSize, bufferUsage,
+                               memPropFlags, _vertexBuffer, _vertexBufferMemory );
+        
 
         // mapping the memory (yang baru saja dialokasikan) to vertex buffer
         void * data;
-        vkMapMemory( _device, _vertexBufferMemory, 0, bufferInfo.size, 0, &data );
-        memcpy( data, vertices.data(), size_t(bufferInfo.size) );
+        vkMapMemory( _device, _vertexBufferMemory, 0, bufferSize, 0, &data );
+        memcpy( data, vertices.data(), size_t(bufferSize) );
         vkUnmapMemory( _device, _vertexBufferMemory );
-    }
-    int32_t FindProperties( const VkPhysicalDeviceMemoryProperties* pMemoryProperties,
-                            uint32_t memoryTypeBitsRequirement, 
-                            VkMemoryPropertyFlags requiredProperties )
-    {
-        const uint32_t memoryCount = pMemoryProperties->memoryTypeCount;
-        for( uint32_t memoryIndex = 0; memoryIndex < memoryCount; ++memoryIndex )
-        {
-            const uint32_t memoryTypeBits = (1 << memoryIndex);
-            const bool isRequiredMemoryType = memoryTypeBitsRequirement & memoryTypeBits;
-
-            const VkMemoryPropertyFlags properties = pMemoryProperties->memoryTypes[memoryIndex].propertyFlags;
-            const bool hasRequiredProperties = (properties & requiredProperties) == requiredProperties;
-
-            if( isRequiredMemoryType && hasRequiredProperties )
-            {
-                return static_cast<int32_t>(memoryIndex);
-            }
-        }
-
-        return -1;  // failed to find memory type
     }
 
 private:
@@ -117,5 +64,6 @@ private:
 /*
  *  from udemy :
  *      vertex input
+ *      Index Buffer and Staging Buffer
 */
 };
--- utilities.h
+++ utilities.h
@@ -47,3 +47,80 @@ struct Vertex
  *      vertex input
 */
 };
+
+namespace Buffer
+{
+    static int32_t FindProperties( const VkPhysicalDeviceMemoryProperties* pMemoryProperties,
+                            uint32_t memoryTypeBitsRequirement, 
+                            VkMemoryPropertyFlags requiredPropertiesFlags )
+    {
+        const uint32_t memoryCount = pMemoryProperties->memoryTypeCount;
+        for( uint32_t memoryIndex = 0; memoryIndex < memoryCount; ++memoryIndex )
+        {
+            const uint32_t memoryTypeBits = (1 << memoryIndex);
+            const bool isRequiredMemoryType = memoryTypeBitsRequirement & memoryTypeBits;
+
+            const VkMemoryPropertyFlags properties = pMemoryProperties->memoryTypes[memoryIndex].propertyFlags;
+            const bool hasRequiredPropertiesFlags = (properties & requiredPropertiesFlags) == requiredPropertiesFlags;
+
+            if( isRequiredMemoryType && hasRequiredPropertiesFlags )
+            {
+                return static_cast<int32_t>(memoryIndex);
+            }
+        }
+
+        return -1;  // failed to find memory type
+    }
+
+
+    static void CreateBuffer( VkPhysicalDevice physicalDevice, VkDevice device, size_t bufferSize,
+                            VkBufferUsageFlags bufferUsage, VkMemoryPropertyFlags memPropFlags,
+                            VkBuffer& buffer, VkDeviceMemory& bufferMemory )
+    {
+        // buffer info (doesn't include assigning memory)
+        VkBufferCreateInfo bufferInfo{};
+        bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        bufferInfo.size = static_cast<uint64_t>( bufferSize );
+        bufferInfo.usage = bufferUsage;
+        bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        bufferInfo.queueFamilyIndexCount = 0;       // optional: just for concurent sharing mode
+        bufferInfo.pQueueFamilyIndices = nullptr;   // optional: just for concurent sharing mode
+        if( vkCreateBuffer( device, &bufferInfo, nullptr, &buffer)
+            != VK_SUCCESS )
+        {
+            throw std::runtime_error( "Failed to create vertex buffer!" );
+        }
+
+        // get memory requirements from vertexBuffer
+        VkMemoryRequirements memReq{};
+        vkGetBufferMemoryRequirements( device, buffer, &memReq );
+
+        // memory allocate info
+        VkMemoryAllocateInfo allocateInfo{};
+        allocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocateInfo.allocationSize = memReq.size;
+
+        VkPhysicalDeviceMemoryProperties memProps{};
+        vkGetPhysicalDeviceMemoryProperties( physicalDevice, &memProps );
+        int32_t memoryType = FindProperties( &memProps, 
+                                            memReq.memoryTypeBits,
+                                            memPropFlags );
+        
+        allocateInfo.memoryTypeIndex = memoryType;
+        if( vkAllocateMemory( device, &allocateInfo, nullptr, &bufferMemory)
+            != VK_SUCCESS )
+        {
+            throw std::runtime_error( "Failed to allocate memory for vertex buffer!" );
+        }
+
+        // binding vertex buffer with the memory allocation
+        vkBindBufferMemory( device, buffer, bufferMemory, 0 );
+    }
+
+/*
+ *  from udemy :
+ *      Index Buffer and Staging Buffer
+*/
+
+}
+
